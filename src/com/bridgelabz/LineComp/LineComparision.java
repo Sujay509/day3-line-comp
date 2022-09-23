9c7cf4ee7f7abea055278e6a6aceb5b0ffb83cee
Sujay509 <sujaychauhan98@gmail.com>
2022-09-23 15:50:58 +0530

    Added : Length of a Line

---
+++ src/com/bridgelabz/LineComp/LineComparision.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.LineComp;
+
+public class LineComparision {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Computation Program");
+	}
+
+}
